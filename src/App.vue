7d8979b157af68b7a16db16c7c56a0f873ebccc0
kai.qiao <kai.qiao@shopee.com>
2023-12-08 10:56:52 +0800

    using custom login button

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-import { decodeCredential } from 'vue3-google-login'
+import type { CallbackTypes } from 'vue3-google-login'
+import { googleAuthCodeLogin } from "vue3-google-login"
+const login = () => {
+  googleAuthCodeLogin().then((response) => {
+    console.log("Handle the response", response)
+  })
+}
 
-const callback = (response) => {
+const callback: CallbackTypes.CredentialCallback = (response) => {
   // This callback will be triggered when the user selects or login to
   // his Google account from the popup
+  console.log("Credential JWT string", response.credential)
 
-  console.log("Handle the response", response)
-  const userData = decodeCredential(response.credential)
-  console.log("Handle the userData", userData)
+  // console.log("Handle the response", response)
+  // const userData = decodeCredential(response.credential)
+  // console.log("Handle the userData", userData)
 }
 </script>
 
@@ -26,8 +33,16 @@ const callback = (response) => {
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
-      <GoogleLogin :callback="callback" />
-
+      <!-- <GoogleLogin :callback="callback" /> -->
+      <!-- <GoogleLogin :callback="callback">
+        <button>Login Using Google</button>
+      </GoogleLogin> -->
+      <button
+        class="google-login-btn"
+        @click="login"
+      >
+        <span class="google-icon"></span> Login Using Google
+      </button>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
@@ -98,6 +113,34 @@ nav a:first-of-type {
 
     padding: 1rem 0;
     margin-top: 1rem;
+  }
+
+  .google-login-btn {
+    padding: 10px 20px;
+    font-size: 16px;
+    background-color: #4285f4;
+    /* Google 蓝色 */
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+  }
+
+  .google-login-btn:hover {
+    background-color: #3c78dc;
+    /* 鼠标悬停时的颜色 */
+  }
+
+  .google-icon::before {
+    content: '\e8d4';
+    /* Google Material Icons 的 Google 图标代码 */
+    font-family: 'Material Icons';
+    position: absolute;
+    left: 10px;
+    top: 50%;
+    transform: translateY(-50%);
+    font-size: 24px;
   }
 }
 </style>
